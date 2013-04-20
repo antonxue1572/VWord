05a616ee26ca9e70453655b126bce88cfa3ab854
unknown <antonxue1572@gmail.com>
2013-04-20 00:04:28 -0400

    quick save

---
+++ src/gui/Frame.java
@@ -0,0 +1,25 @@
+package gui;
+
+import java.awt.Container;
+import java.awt.Insets;
+import javax.swing.JFrame;
+
+public class Frame {
+    
+    // Framework
+    private JFrame frame;
+    private Container pane;
+    private Insets insets;
+    
+    // Constructor
+    public Frame() {
+        
+    }
+    
+    // Begins stuff
+    public void initGUI() {
+        // Frame
+        frame = new JFrame("VWord ")
+    }
+    
+}
